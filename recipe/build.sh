ee75b8f3a80dc0b003d8459a345b2a17e892de77
Jonathan Helmus <jjhelmus@gmail.com>
2018-01-10 17:47:25 -0600

    revert to version 2.2.31
    Revert to version 2.2.31
    Remove run requirements which are provided by run_exports

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,13 +1,8 @@
 #!/usr/bin/env bash
+
 mkdir build
-git checkout concat # temporary testing
 cd build
-# export LIBHDFS3_HOME=`pwd`
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    ../libhdfs3/bootstrap --prefix=$PREFIX --dependency=$PREFIX --enable-libc++
-else
-    ../libhdfs3/bootstrap --prefix=$PREFIX --dependency=$PREFIX 
-fi
-make
+../bootstrap --prefix=$PREFIX --dependency=$PREFIX
+make -j${CPU_COUNT}
 make install
 rm $PREFIX/lib/libhdfs3.a  # don't need static
