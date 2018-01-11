1c1770f3c03abae700aca01adacd8853037fc922
Jonathan Helmus <jjhelmus@gmail.com>
2018-01-11 15:44:51 -0600

    WIP

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,7 @@
 
 mkdir build
 cd build
-../bootstrap --prefix=$PREFIX --dependency=$PREFIX
+../libhdfs3/bootstrap --prefix=$PREFIX --dependency=$PREFIX
 make -j${CPU_COUNT}
 make install
 rm $PREFIX/lib/libhdfs3.a  # don't need static
